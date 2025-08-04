b7a407466506b87912301605824c9e0a900e50f7
Aral <pro.aralars@gmail.com>
2025-08-04 20:40:42 +0200

    Fix FizzBuzz logic, naming, layout. changed until => to.

--- src/main/scala/exercices/fizzBuzz.scala
+++ src/main/scala/exercices/fizzBuzz.scala
@@ -8,8 +8,8 @@ package exercices
 
 object FizzBuzz {
   def mutable(): Unit =
-    for (i <- 1 until 100) {
-      if (i % 5 == 0) println("FizzBuzz")
+    for (i <- 1 to 100) {
+      if (i % 15 == 0) println("FizzBuzz")
       else if (i % 3 == 0) println("Fizz")
       else if (i % 5 == 0) println("Buzz")
       else println(i)
@@ -17,9 +17,9 @@ object FizzBuzz {
 
   def functional(): Unit =
     (1 to 100).map {
-      case i if i % 3 == 0 => "FizzBuzz"
-      case i if i % 5 == 0 => "Buzz"
-      case i if i % 3 == 0 => "Fizz"
+      case i if i % 15 == 0 => "FizzBuzz"
+      case i if i % 3 == 0  => "Buzz"
+      case i if i % 5 == 0  => "Fizz"
       case i => i.toString
     }.foreach(println)
 }
