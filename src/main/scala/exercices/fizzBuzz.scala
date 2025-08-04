32580dd1b3c746e3322664ce4a83d1fd62752dbd
Aral <pro.aralars@gmail.com>
2025-08-04 20:37:59 +0200

    Refactor FizzBuzz example to separate mutable and functional implementations into an object

--- src/main/scala/exercices/fizzBuzz.scala
+++ src/main/scala/exercices/fizzBuzz.scala
@@ -1,26 +1,25 @@
 package exercices
 
-object fizzBuzz extends App {
+@main def runFizzBuzz(): Unit =
+  println("mutable style")
+  FizzBuzz.mutable()
+  println("\nfunctional style")
+  FizzBuzz.functional()
 
-  def mutableStyleFizzBuzz(): Unit = {
+object FizzBuzz {
+  def mutable(): Unit =
     for (i <- 1 until 100) {
       if (i % 5 == 0) println("FizzBuzz")
       else if (i % 3 == 0) println("Fizz")
       else if (i % 5 == 0) println("Buzz")
       else println(i)
     }
-  }
 
-  def functionalStyleFizzBuzz(): Unit = {
+  def functional(): Unit =
     (1 to 100).map {
       case i if i % 3 == 0 => "FizzBuzz"
       case i if i % 5 == 0 => "Buzz"
-      case i if i % 3 == 1 => "Fizz"
+      case i if i % 3 == 0 => "Fizz"
       case i => i.toString
     }.foreach(println)
-  }
-
-  mutableStyleFizzBuzz()
-  println("\n===========\n")
-  functionalStyleFizzBuzz()
 }
